d32c3616ff133489bfee636f86ebbfa32c4d1d80
Peti0 <113418512+Peti0@users.noreply.github.com>
2023-03-20 08:30:53 +0100

    Add files via upload

---
+++ Ponthatarok/Ponthatarok/Ponthat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponthatarok
+{
+    internal class PontHat
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem a pontszámot:");
+            int pont = int.Parse(Console.ReadLine());
+
+            if (pont > 0 && pont < 100)
+            {
+                PontHat p1 = new PontHat(pont);
+                p1.szamitas();
+                Console.WriteLine("A kapott eredmény{0}", pont);
+            }
+            else Console.WriteLine("Rossz adat");
+                Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Ponthatarok/Ponthatarok/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponthatarok
+{ 
+    struct PontAdat
+    {
+        public string pont;
+    }
+    internal class Program
+    {
+        private int pontok;
+        private static string[] jegyek = { "elégtelen", "elégséges", "közepes", "jó", "jeles" };
+        private int pont = 0;
+
+        public PontHat(int[] k)
+        {
+            this.pontok = k;
+        }
+
+
+        public PontAdat GetOsztalyzat()
+        {
+            return jegyek[jegy - 1];
+        }
+        public void szamitas()
+        {
+            setPontAdat();
+        }
+        public void setPontAdat()
+        {
+            if (this.pont >= 0 && this.pont < 50) this.pontok = 1;
+            else if (this.pont >= 50 && this.pont < 65) this.pontok = 2;
+            else if (this.pont >= 65 && this.pont < 80) this.pontok = 3;
+            else if (this.pont >= 80 && this.pont < 90) this.pontok = 4;
+            else if (this.pont >= 90 && this.pont < 65) this.pontok = 5;
+            else this.pontok = 0;
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
