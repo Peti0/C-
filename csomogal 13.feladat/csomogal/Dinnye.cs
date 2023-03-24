a26b1ed7471a084e003729c8fde743362a889e6a
Peti0 <113418512+Peti0@users.noreply.github.com>
2023-03-24 07:53:30 +0100

    Add files via upload

---
+++ csomogal 13.feladat/csomogal/Dinnye.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csomogal
+{
+    internal class Dinnye
+    {
+        private int darab;
+        private int atmero;
+        private double anyag;
+        
+        public Dinnye() { }
+
+        public void setDarab(int d)
+        {
+            this.darab = d;
+        }
+
+        public void setAtmero(int a)
+        {
+            this.atmero = a;
+        }
+
+
+
+
+        public void Szamol()
+        {
+            this.anyag =this.darab * (this.atmero * Math.PI + 60);
+        }
+
+        public double getAnyag() { return this.anyag; }
+    }
+}
---
+++ csomogal 13.feladat/csomogal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csomogal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg a dinnyék átmérojet ");
+            int atmero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg a dinnyék darabját");
+            int darab = int.Parse(Console.ReadLine());
+
+            Dinnye c1= new Dinnye();
+            c1.setAtmero(atmero);
+            c1.setDarab(darab);
+            c1.Szamol();
+            double eredmeny = c1.getAnyag();
+           
+            Console.WriteLine("A dinnyék becsomagalásához {0} cm kell", eredmeny);
+
+            Console.ReadKey();  
+        }
+    }
+}
