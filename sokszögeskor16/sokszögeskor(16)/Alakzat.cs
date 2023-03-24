0b44903918fbd11a11c2b22dfc2eda73693d75ca
Peti0 <113418512+Peti0@users.noreply.github.com>
2023-03-24 10:24:31 +0100

    Add files via upload

---
+++ sokszögeskor16/sokszögeskor(16)/Alakzat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace haromszögbeirhatokor_16_
+{
+    internal class Alakzat
+    {
+        private int a;
+        private int b;
+        private int c;
+        private double terulet;
+        private int kerulets;
+        private double sugar;
+        public Alakzat() { }
+
+        public void setA(int A) { this.a = A; }
+        public void setB(int B) { this.b = B; }
+        public void setC(int B) { this.c = B; }  
+        public void setTerulet(int Terulet) { this.terulet = Terulet; }
+        public void setKerulets(int KeruletS) { this.kerulets = KeruletS; }
+        public void setSugar(int sugar) { this.sugar = sugar; }
+
+        public void Szamol()
+        {
+            this.kerulets = (this.a + this.b + this.c) / 2;
+
+            this.terulet = this.kerulets * (this.kerulets - this.a) * (this.kerulets - this.b) * (this.kerulets - this.c);
+
+            this.terulet = Math.Sqrt(this.terulet);
+
+            this.sugar = 2 * this.terulet / (this.a + this.b + this.c);
+        }
+
+        public double getSugar() { return this.sugar; }
+    }
+}
---
+++ sokszögeskor16/sokszögeskor(16)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace haromszögbeirhatokor_16_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Háromszögbe írható kör sugara:");
+            Console.WriteLine("Adja meg a háromszög A oldalát.");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg a háromszög B oldalát.");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg a háromszög C oldalát.");
+            int c = int.Parse(Console.ReadLine());
+
+            Alakzat c1 = new Alakzat();
+            c1.setA(a); c1.setB(b); c1.setC(c);
+            double sugar = c1.getSugar();
+            c1.Szamol();
+
+            Console.WriteLine("A háromszögbe írható kör sugara: {0}", sugar);
+            Console.ReadKey();
+        }
+    }
+}
