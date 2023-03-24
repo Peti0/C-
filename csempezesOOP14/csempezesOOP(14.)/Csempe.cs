7d50675e5755c5e47eacb890e0c91eb0f73f9b4a
Peti0 <113418512+Peti0@users.noreply.github.com>
2023-03-24 10:23:37 +0100

    Add files via upload

---
+++ csempezesOOP14/csempezesOOP(14.)/Csempe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csempezesOOP_14._
+{
+    internal class Csempe
+    {
+        public int szelesseg;
+        public int magassag;
+        public double csempe;
+        public Csempe() { }
+        
+        public void setSzelesseg(int sz) { this.szelesseg= sz; }
+        public void setMagassag(int m) { this.magassag= m; }
+
+        public void Szamol()
+        {
+            this.csempe = (this.magassag * 100 * this.magassag * 100) / (20 * 20) * 1.1;
+            if (this.csempe % 1 !=0)
+            {
+                this.csempe = this.csempe - this.csempe % 1 + 1;
+            }
+        }
+
+        
+
+        public double getCsempe() { return this.csempe; }
+    }
+}
---
+++ csempezesOOP14/csempezesOOP(14.)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csempezesOOP_14._
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg a terület szélességét méterben:");
+                int szelesseg =  int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg a terület magasságát méterben:");
+                int magassag = int.Parse(Console.ReadLine());
+
+            Csempe c1 = new Csempe();
+            c1.setMagassag(magassag);
+            c1.setSzelesseg(szelesseg);
+            c1.Szamol();
+            double csempe = c1.getCsempe();
+            Console.WriteLine("A terület becsempézéséhez {0} db csempére lesz szükség.", csempe);
+            Console.ReadKey();
+        }
+    }
+}
