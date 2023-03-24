bd8086310fa5c8238630848fd2cd706474ff947c
Peti0 <113418512+Peti0@users.noreply.github.com>
2023-03-24 10:24:03 +0100

    Add files via upload

---
+++ sokszögek 15/sokszögek(15)/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sokszögek_15_
+{
+    internal class Szog
+    {
+        private int szog;
+        private int atlo;
+       
+
+        public Szog() { }
+
+        public void setSzog(int szog) { this.szog = szog; }
+
+        public void Szamol() 
+        {
+            this.atlo = this.szog * (this.szog-3) / 2;
+        }
+        public int getAtlo() { return this.atlo; }  
+    }
+}
---
+++ sokszögek 15/sokszögek(15)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sokszögek_15_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Átlószámító");
+            Console.WriteLine("Adja meg a szögek számát:");
+            int szog = int.Parse(Console.ReadLine());
+
+            Szog c1 = new Szog();
+            c1.setSzog(szog);
+            c1.Szamol();
+            int atlo = c1.getAtlo();
+
+            Console.WriteLine("A(z) {0} szöggel rendelkező alakzatnak {1} db átlója van.",szog , atlo);
+
+            Console.ReadKey();
+        }
+    }
+}
