4a2fedecf9fe760c3b5db8573f29b1daead47436
Peti0 <113418512+Peti0@users.noreply.github.com>
2023-03-27 13:35:56 +0200

    Add files via upload

---
+++ masoperc 17/oraperc masodperc/MP.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OraPercMp
+{
+    internal class MP
+    {
+        public int a, b, c, d, q, x, y, z, eredmeny;
+        public MP() { }
+        public MP(int a, int b, int c, int d, int q, int x, int y, int z, int eredmeny)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.q = q;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.eredmeny = eredmeny;
+        }
+
+        public void setA(int a)
+        {
+            this.a = a;
+        }
+        public void setB(int b)
+        {
+            this.b = b;
+        }
+        public void setC(int c)
+        {
+            this.c = c;
+        }
+        public void setD()
+        {
+            this.d = this.a * 3600 + this.b * 60 + this.c;
+        }
+        public void setQ(int q)
+        {
+            this.q = q;
+        }
+        public void setX(int x)
+        {
+            this.x = x;
+        }
+        public void setY(int y)
+        {
+            this.y = y;
+        }
+        public void setZ()
+        {
+            this.z = this.q * 3600 + this.x * 60 + this.y;
+        }
+        public void setEredmeny()
+        {
+            if (this.d > this.z)
+            {
+                this.eredmeny = this.d - this.z;
+                Console.WriteLine("A két időpont között eltelt {0} másodperc", this.eredmeny);
+            }
+            else
+            {
+                this.eredmeny = this.z - this.d;
+                Console.WriteLine("A két időpont között eltelt {0} másodperc", this.eredmeny);
+            }
+        }
+    }
+}
---
+++ masoperc 17/oraperc masodperc/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OraPercMp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, q, x, y;
+            Console.WriteLine("Add meg az  első órát: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg az  első percet: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg az  első másodpercet: ");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg a második órát : ");
+            q = int.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg a második percet : ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg a második másodpercet: ");
+            y = int.Parse(Console.ReadLine());
+            MP c1 = new MP();
+            c1.setA(a);
+            c1.setB(b);
+            c1.setC(c);
+            c1.setD();
+            c1.setQ(q);
+            c1.setX(x);
+            c1.setY(y);
+            c1.setZ();
+            c1.setEredmeny();
+            Console.ReadKey();
+        }
+    }
+}
